dc42a23c1d005a319554dc839a8d7f5411980d27
SHIVU S <125435358+SHIVASHIVU01@users.noreply.github.com>
2024-11-16 21:21:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+//Clock
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+// Initialize the clock immediately on load
+updateClock();
